e68126f1b4ea072a48827e64c9ce19a2db9e3661
mabdelhamid <mabdelhamid@aneo.fr>
2017-03-07 23:18:00 +0100

    add spring boot bootstraping

---
+++ hero-api/src/main/java/fr/aneo/game/HeroApplication.java
@@ -0,0 +1,15 @@
+package fr.aneo.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by raouf on 07/03/17.
+ */
+@SpringBootApplication
+public class HeroApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(HeroApplication.class, args);
+    }
+}
