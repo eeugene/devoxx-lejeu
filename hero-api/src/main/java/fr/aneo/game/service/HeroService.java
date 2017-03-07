1f1023def4847e232ac4e107d322804cb0b20038
mabdelhamid <mabdelhamid@aneo.fr>
2017-03-07 16:50:38 +0100

    add authentication service

--- hero-api/src/main/java/fr/aneo/game/service/HeroService.java
+++ hero-api/src/main/java/fr/aneo/game/service/HeroService.java
@@ -2,17 +2,25 @@ package fr.aneo.game.service;
 
 import fr.aneo.game.model.Hero;
 import fr.aneo.game.repository.HeroRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by raouf on 04/03/17.
  */
+@Service
+@Slf4j
 public class HeroService {
 
     @Autowired
     private HeroRepository heroRepository;
+
+    public Hero findHeroByEmail(String email) {
+        return heroRepository.findOne(email);
+    }
 
     public List<Hero> findAllHeroes() {
         return heroRepository.findAll();
