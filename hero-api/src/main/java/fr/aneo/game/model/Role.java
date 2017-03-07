fb1adce6529eee8f94bde677d8d37324e2df4516
mabdelhamid <mabdelhamid@aneo.fr>
2017-03-07 19:00:06 +0100

    remove useless imports

--- hero-api/src/main/java/fr/aneo/game/model/Role.java
+++ hero-api/src/main/java/fr/aneo/game/model/Role.java
@@ -1,15 +1,5 @@
 package fr.aneo.game.model;
 
-import lombok.*;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-import java.util.Enumeration;
-
-import static javax.persistence.GenerationType.AUTO;
-
 /**
  * Created by raouf on 05/03/17.
  */
