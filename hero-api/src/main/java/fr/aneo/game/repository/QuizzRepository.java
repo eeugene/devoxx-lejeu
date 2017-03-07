7cd248afebbaf2a8af5abd1dba9c4485274bc5d8
mabdelhamid <mabdelhamid@aneo.fr>
2017-03-07 23:17:19 +0100

    transactional crud

--- hero-api/src/main/java/fr/aneo/game/repository/QuizzRepository.java
+++ hero-api/src/main/java/fr/aneo/game/repository/QuizzRepository.java
@@ -2,9 +2,11 @@ package fr.aneo.game.repository;
 
 import fr.aneo.game.model.Quizz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by raouf on 05/03/17.
  */
+@Transactional
 public interface QuizzRepository extends JpaRepository<Quizz, Long> {
 }
