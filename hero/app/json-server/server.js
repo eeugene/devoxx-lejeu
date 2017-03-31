b5deb317f6aab4381805536bfced42f09c819d75
emmanueleugene <emmanueleugene@gmail.com>
2017-03-31 23:23:50 +0200

    Add Express Server for prod bundle

---
+++ hero/app/json-server/server.js
@@ -0,0 +1,46 @@
+var path = require('path')
+
+var jsonServer = require('json-server')
+var server = jsonServer.create()
+var router = jsonServer.router(path.join(__dirname, 'db.json'))
+var middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+server.use(function (req, res, next) {
+  if (req.method === 'POST') {
+    // Converts POST to GET and move payload to query params
+    // This way it will make JSON Server that it's GET request
+    req.method = 'GET'
+    req.query = req.body
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+server.get('/api/login', function (req, res, next) {
+  let credentials =JSON.parse(Object.keys(req.query)[0])
+  if (credentials.username == '')
+  {
+    res.sendStatus(401)
+  } else {
+    next()
+  }
+})
+
+server.get('/api/register', function (req, res, next) {
+  let credentials =JSON.parse(Object.keys(req.query)[0])
+  if (credentials.username == '')
+  {
+    res.sendStatus(401)
+  } else {
+    next()
+  }
+})
+
+server.use('/api',router)
+
+server.listen(3000, function () {
+  console.log('JSON Server is running')
+})
--- hero/app/server/server.js
+++ hero/app/server/server.js
@@ -1,46 +1,16 @@
-var path = require('path')
+var express = require('express');
+var path = require('path');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer();
+var app = express();
 
-var jsonServer = require('json-server')
-var server = jsonServer.create()
-var router = jsonServer.router(path.join(__dirname, 'db.json'))
-var middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-
-server.use(jsonServer.bodyParser)
-server.use(function (req, res, next) {
-  if (req.method === 'POST') {
-    // Converts POST to GET and move payload to query params
-    // This way it will make JSON Server that it's GET request
-    req.method = 'GET'
-    req.query = req.body
-  }
-  // Continue to JSON Server router
-  next()
-})
-
-server.get('/api/login', function (req, res, next) {
-  let credentials =JSON.parse(Object.keys(req.query)[0])
-  if (credentials.username == '')
-  {
-    res.sendStatus(401)
-  } else {
-    next()
-  }
-})
-
-server.get('/api/register', function (req, res, next) {
-  let credentials =JSON.parse(Object.keys(req.query)[0])
-  if (credentials.username == '')
-  {
-    res.sendStatus(401)
-  } else {
-    next()
-  }
-})
-
-server.use('/api',router)
-
-server.listen(3000, function () {
-  console.log('JSON Server is running')
-})
+app.use('/', express.static(__dirname + '/'));
+app.all('/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080'
+    });
+  });
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
+app.listen(80);
