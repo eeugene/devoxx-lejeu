4b8c8533fbf9b7b0a1dbb401eb57b02ac6f26256
emmanueleugene <emmanueleugene@gmail.com>
2017-03-31 19:43:16 +0200

    Fix SQL setup script

--- setup.sql
+++ setup.sql
@@ -1,9 +1,7 @@
-CREATE OR REPLACE USER 'aneo'@'localhost' IDENTIFIED BY 'devoxx2017';
+CREATE USER 'aneo'@'localhost' IDENTIFIED BY 'devoxx2017';
 
 CREATE DATABASE herodb;
-
-GRANT CREATE, INSERT, SELECT, DELETE, UPDATE ON herodb.* to 'aneo'@'localhost' IDENTIFIED BY 'devoxx2017';
-
 CREATE DATABASE arenadb;
 
-GRANT CREATE, INSERT, SELECT, DELETE, UPDATE ON arenadb.* to 'aneo'@'localhost' IDENTIFIED BY 'devoxx2017';
+GRANT ALTER, REFERENCES, CREATE, INSERT, SELECT, DELETE, UPDATE ON herodb.* to 'aneo'@'localhost' IDENTIFIED BY 'devoxx2017';
+GRANT ALTER, REFERENCES, CREATE, INSERT, SELECT, DELETE, UPDATE ON arenadb.* to 'aneo'@'localhost' IDENTIFIED BY 'devoxx2017';
