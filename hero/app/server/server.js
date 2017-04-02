5914c0ce8a8961c281be42d31fe0f825c87e4ce2
emmanueleugene <emmanueleugene@gmail.com>
2017-04-02 23:22:54 +0200

    Fix Express Route for statistics

--- hero/app/server/server.js
+++ hero/app/server/server.js
@@ -2,21 +2,27 @@ var express = require('express');
 var path = require('path');
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer();
+var proxyStats = httpProxy.createProxyServer({
+        ignorePath:true
+});
 var app = express();
 
 app.use('/',  express.static('../dist/'));
-app.all('/*', function (req, res) {
+app.all('/api/*', function (req, res) {
+    //console.log('in /api/*');
     proxy.web(req, res, {
         target: 'http://localhost:8080'
     });
 });
 app.all('/leaderboard', function (req, res) {
+    console.log('in /leaderboard');
     proxy.web(req, res, {
         target: 'http://localhost:8081'
     });
 });
 app.all('/statistics', function (req, res) {
-    proxy.web(req, res, {
+    console.log('in /statistics');
+    proxyStats.web(req, res, {
         target: 'http://localhost:8083'
     });
 });
